276ffa7f0fe1730b731fa0880e6d9ddd5794d2db
Martin Hanewald <martin.hanewald@gmail.com>
2020-12-21 20:37:23 +0000

    add requirements

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Martin Hanewald",
     author_email='martin.hanewald@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        #'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
